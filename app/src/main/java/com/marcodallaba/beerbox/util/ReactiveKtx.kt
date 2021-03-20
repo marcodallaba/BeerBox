f81843fc4a435981e47634ec8e3a7c469f67dbfe
Marco Dalla Ba <marco.dallaba@nexi.it>
2021-03-20 19:50:01 +0100

    Fixed deprecation

--- app/src/main/java/com/marcodallaba/beerbox/util/ReactiveKtx.kt
+++ app/src/main/java/com/marcodallaba/beerbox/util/ReactiveKtx.kt
@@ -1,29 +1,29 @@
 package com.marcodallaba.beerbox.util
 
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.GenericLifecycleObserver
-import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.*
 import androidx.lifecycle.Lifecycle.Event.*
 import androidx.lifecycle.Lifecycle.State.*
-import androidx.lifecycle.LifecycleOwner
 import io.reactivex.disposables.Disposable
 
 fun Disposable.bindToLifecycle(lifecycleOwner: LifecycleOwner): Disposable {
     val lifecycle = ((lifecycleOwner as? Fragment)?.viewLifecycleOwner ?: lifecycleOwner).lifecycle
     val subscriptionState = lifecycle.currentState
-    lifecycle.addObserver(GenericLifecycleObserver { _, e ->
+    lifecycle.addObserver(LifecycleEventObserver { _, e ->
         if (shouldDispose(subscriptionState, e))
             try {
                 dispose()
             } catch (_: Exception) {
+                //ignore
             }
     })
     return this
 }
 
-private fun shouldDispose(subscriptionState: Lifecycle.State, stateEvent: Lifecycle.Event?) = when (stateEvent) {
-    ON_PAUSE -> subscriptionState == STARTED || subscriptionState == RESUMED
-    ON_STOP -> subscriptionState == CREATED
-    ON_DESTROY -> true
-    else -> false
-}
+private fun shouldDispose(subscriptionState: Lifecycle.State, stateEvent: Lifecycle.Event?) =
+    when (stateEvent) {
+        ON_PAUSE -> subscriptionState == STARTED || subscriptionState == RESUMED
+        ON_STOP -> subscriptionState == CREATED
+        ON_DESTROY -> true
+        else -> false
+    }
